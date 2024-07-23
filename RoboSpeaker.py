b2041892986bf68e402fd8336f613e92b8e4530d
Ammu <anagharanu002@gmail.com>
2024-07-23 05:38:50 +0530

    created the robo speaker succesfully

--- RoboSpeaker.py
+++ RoboSpeaker.py
@@ -1,5 +1,18 @@
-a = 10
-print(a)
+import os
 
-k=25
-print(k)
+def speak(text):
+    command = f'powershell -Command "Add-Type -AssemblyName System.Speech; ' \
+              f'(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'{text}\');"'
+    os.system(command)
+
+if __name__ == '__main__':
+    print("Welcome to RoboSpeaker 1.1. created by Anagha")
+    while True:
+        x = input("Enter what you want to speak: ")
+        if x.lower() == 'q':
+            speak("Okay, bye bye friend!")
+            break
+        speak(x)
+
+        
+    
